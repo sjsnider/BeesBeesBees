5726f3c0f8521f92e9fee5aee0c58b820550059c
Scott Snider <sjsnider@gmail.com>
2014-04-22 22:14:56 -0700

    pass all tests

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
 var ForagerBee = function() {
+	Bee.call(this);
+	this.age = 10;
+	this.job = 'find pollen';
+	this.canFly = true;
+	this.treasureChest = [];
 };
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor =  ForagerBee;
 
+ForagerBee.prototype.forrage = function(value){
+	this.treasureChest.push(value);
+};
--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,16 @@
 var RetiredForagerBee = function() {
+	ForagerBee.call(this);
+	this.age = 40;
+	this.job = 'gamble';
+	this.canFly = false;
+	this.color = 'grey';
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+RetiredForagerBee.prototype.forrage = function(){
+	return "I am too old, let me play cards instead";
+};	
+RetiredForagerBee.prototype.gamble = function(){
+	this.treasureChest.push(5);
+};	
